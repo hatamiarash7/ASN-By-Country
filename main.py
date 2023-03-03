c4ab8019ac3e7b13b4f16a1a84bb62ecd057d039
Arash Hatami <hatamiarash7@gmail.com>
2023-03-04 00:21:30 +0330

    Lint code

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 """
 
 import argparse
+import warnings
+import time
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from rich.console import Console
 from rich.progress import track
-import warnings
-import time
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -20,7 +20,7 @@ args = parser.parse_args()
 
 country = args.country.upper()
 
-# Create object page
+# Create object page - pylint: disable=line-too-long
 url = f"https://www-public.imtbs-tsp.eu/~maigron/RIR_Stats/RIR_Delegations/Delegations/ASN/{country}.html"
 response = requests.get(url)
 response.raise_for_status()
@@ -42,8 +42,8 @@ with open(file='ranges.txt', mode='w', encoding='UTF-8') as ranges_file:
         row_data = row.find_all('td')
         row = [i.text for i in row_data]
         if row[6] == 'Allocated':
-            sep = ',' if row_index != len(rows) - 1 else ''
-            ranges_file.write(row[3] + sep)
+            SEP = ',' if row_index != len(rows) - 1 else ''
+            ranges_file.write(row[3] + SEP)
         data = data.append(dict(zip(headers[1:], row)), ignore_index=True)
         time.sleep(0.002)
     console.log(f"Found\t[green]{len(data)}[/green] ASN's")
