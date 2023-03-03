6ec5e837b35ed6876be5408d9b58cea1c6c64121
Arash Hatami <hatamiarash7@gmail.com>
2023-03-04 00:16:17 +0330

    Improve code

--- main.py
+++ main.py
@@ -1,56 +1,53 @@
 """This is a simple script to get all ASN's of a given country
 """
 
-import sys
+import argparse
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from rich.console import Console
+from rich.progress import track
+import warnings
+import time
 
-console = Console(log_path=False)
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-if len(sys.argv) != 2:
-    console.log("Usage: python main.py <country code>")
-    sys.exit(1)
+console = Console()
 
-country = sys.argv[1]
+parser = argparse.ArgumentParser(description="Get ASN's of a given country")
+parser.add_argument("country", help="Country code")
+args = parser.parse_args()
+
+country = args.country.upper()
 
 # Create object page
-url = 'https://www-public.imtbs-tsp.eu/~maigron/RIR_Stats/RIR_Delegations/Delegations/ASN/'\
-    + country + '.html'
-page = requests.get(url)
-
-if page.status_code != 200:
-    console.log(
-        f'Error - {page.status_code} : Did you enter the correct country?')
-    sys.exit(1)
+url = f"https://www-public.imtbs-tsp.eu/~maigron/RIR_Stats/RIR_Delegations/Delegations/ASN/{country}.html"
+response = requests.get(url)
+response.raise_for_status()
 
 # Obtain page's information
-soup = BeautifulSoup(page.text, 'lxml')
+soup = BeautifulSoup(response.text, 'lxml')
 
 # Obtain information from tag <table>
 table = soup.find('table', attrs={'class': 'delegs ripencc'})
 
 # Obtain headers
-headers = []
-for header in table.find_all('th'):
-    title = header.text
-    headers.append(title)
-
+headers = [header.text for header in table.find_all('th')]
 
 # Create a data frame
 data = pd.DataFrame(columns=headers[1:])
-with open(file='ranges.txt', mode='w', encoding='UTF-8') as ranges:
+with open(file='ranges.txt', mode='w', encoding='UTF-8') as ranges_file:
     rows = table.find_all('tr')[2:]
-    for j in rows:
-        with console.status("[bold green]Reading ...", spinner='aesthetic') as status:
-            row_data = j.find_all('td')
-            row = [i.text for i in row_data]
-            console.log(f"[red]Find[/red] \t [blue]{row[3]}[/blue]")
-            if row[6] == 'Allocated':
-                ranges.write(row[3] + (',' if j != rows[-1] else ''))
-            length = len(data)
-            data.loc[length] = row
+    for row_index, row in enumerate(track(rows, description="Reading ...")):
+        row_data = row.find_all('td')
+        row = [i.text for i in row_data]
+        if row[6] == 'Allocated':
+            sep = ',' if row_index != len(rows) - 1 else ''
+            ranges_file.write(row[3] + sep)
+        data = data.append(dict(zip(headers[1:], row)), ignore_index=True)
+        time.sleep(0.002)
+    console.log(f"Found\t[green]{len(data)}[/green] ASN's")
+
 
 # Export to csv
 data.to_csv('asn_list.csv', index=False)
